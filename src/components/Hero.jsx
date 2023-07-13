89ebfa9ef167292d23a780e8abb33d7b04a95483
Christopher Bellocci <christopher.bellocci@hotmail.com>
2023-07-13 12:07:07 +0100

    updated gh link direct to corresponding repo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
         <button
           type='button'
-          onClick={() => window.open('https://github.com/cbelloxxi')}
+          onClick={() => window.open('https://github.com/CBelloxxi/OpenAI-Article-Summarizer')}
           className='black_btn'
         >
           Github
